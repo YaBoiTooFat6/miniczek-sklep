f3058181bb3e3ea1565c8bc1ca9e1f81a2ecc218
YaBoiTooFat6 <yaboitoobig@gmail.com>
2026-05-25 10:26:06 +0200

    Add files via upload

--- db.php
+++ db.php
@@ -1,14 +1,14 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "login_system";
+$host = "mysql8";
+$user = "01493838_dominik";
+$password = "qqqqqq123";
+$database = "01493838_dominik";
 
 $conn = mysqli_connect($host,$user,$password,$database);
 
 if($conn == false){
-    die("blad polaczenia");
+    die("blad polacddia");
 }
 
 ?>
